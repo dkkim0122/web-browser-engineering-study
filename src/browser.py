87ee789281e29f355bae91239f0f0bb10e5d9c3a
dokyung-kim <dkkim0122@gmail.com>
2025-11-08 17:41:26 +0900

    파이썬을 이용한 HTML 표시, HTTPS 기초 이해

--- src/browser.py
+++ src/browser.py
@@ -1,14 +1,30 @@
 import socket
+import ssl
 
 class URL:
   def __init__(self, url):
     self.scheme, url = url.split("://", 1)
-    assert self.scheme == "http" # 일단 http만 지원하도록 함
+    assert self.scheme in ["http", "https"] # 일단 http와 https만 지원하도록 함
 
     if "/" not in url:
       url = url + "/"
     self.host, url = url.split("/", 1)
     self.path = "/" + url
+
+    if ":" in self.host:
+      self.host, port = self.host.split(":", 1)
+      self.port = int(port)
+    
+    # 연결에 따른 포트 지정
+    if self.scheme == "http":
+      self.port = 80
+    elif self.scheme == "https":
+      self.port = 443
+    
+    # 사용자 지정 포트 파싱하기
+    if ":" in self.host:
+      self.host, port = self.host.split(":", 1)
+      self.port = int(port)
 
   def request(self):
     # 소켓 생성
@@ -19,7 +35,11 @@ class URL:
     )
 
     # 소켓 연결
-    s.connect((self.host, 80))
+    s.connect((self.host, self.port))
+
+    if self.scheme == "https":
+      ctx = ssl.create_default_context()
+      s = ctx.wrap_socket(s, server_hostname=self.host)
 
     request = "GET {} HTTP/1.0\r\n".format(self.path)
     request += "Host: {}\r\n".format(self.host) # 줄바꿈을 위해 \r이 아니라 \r\n을 사용해야 한다.
@@ -49,3 +69,23 @@ class URL:
     body = response.read()
     s.close() # 소켓 닫기
     return body
+  
+def show(body):
+  in_tag = False
+  for c in body:
+    if c == "<":
+      in_tag = True
+    elif c == ">":
+      in_tag = False
+    elif not in_tag:
+      print(c, end="") # 태그 안에 있지 않은 문자만 출력
+
+# 웹페이지 로드
+def load(url):
+  body = url.request()
+  show(body)
+
+# 커맨드 라인에서 실행할 때 load 함수 실행
+if __name__ == "__main__":
+  import sys
+  load(URL(sys.argv[1])) # 커맨드 라인 인수를 URL 객체로 변환하여 load 함수에 전달
